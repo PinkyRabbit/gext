707ff32ff9d6d58201c844d7ea37b144726c3813
Mikita Melnikau <usesa@yandex.com>
2017-10-28 21:46:11 +0300

    before add mutation

--- background.js
+++ background.js
@@ -1,46 +1,69 @@
 chrome.browserAction.setBadgeText({text: "ON"});
-chrome.storage.sync.set({'state': 1, 'sound': true, 'pm': true}, function() {
-  message('Settings saved');
+
+/*
+
+chrome.storage.local.get('state', function(data) {
+  if(!data){
+    chrome.storage.local.set({'state': 1}, function() {
+      message('Settings saved');
+    });
+  }
 });
+chrome.storage.local.get('sound', function(data) {
+  if(!data){
+    chrome.storage.local.set({'sound': true}, function() {
+      message('Settings saved');
+    });
+  }
+});
+chrome.storage.local.get('pm', function(data) {
+  if(!data){
+    chrome.storage.local.set({'pm': true}, function() {
+      message('Settings saved');
+    });
+  }
+});
+*/
 
 
-chrome.browserAction.onClicked.addListener(function (tab){
-  chrome.storage.sync.get('state', function(data) {
-    if (data.state === 0) {
-      chrome.storage.sync.set({'state': 1, 'pm': false }, function(datae){
-        chrome.browserAction.setBadgeText({text: ""});
-        // chrome.tabs.return;
-        chrome.browserAction.setIcon({ path:
-          {
-            "16": "/images/disable16.png",
-            "48": "/images/disable48.png",
-            "128": "/images/disable128.png"
-          }
+// chrome.storage.local.set({'state': 1, 'sound': true, 'pm': true, 'lastmsg': ''}, function() {
+  chrome.browserAction.onClicked.addListener(function (tab){
+    chrome.storage.local.get('state', function(data) {
+      if (data.state === 0) {
+        chrome.storage.local.set({'state': 1, 'pm': false }, function(datae){
+          chrome.browserAction.setBadgeText({text: ""});
+          // chrome.tabs.return;
+          chrome.browserAction.setIcon({ path:
+            {
+              "16": "/images/disable16.png",
+              "48": "/images/disable48.png",
+              "128": "/images/disable128.png"
+            }
+          });
+          chrome.browserAction.setTitle({ title: 'Расширение отключено'});
         });
-        chrome.browserAction.setTitle({ title: 'Расширение отключено'});
-      });
 
-      //do something, removing the script or whatever
-    } else if (data.state === 1){
-      chrome.storage.sync.set({'state': 2, 'sound': true, 'pm': true}, function() {
-        chrome.browserAction.setBadgeText({text: "ON"});
-        chrome.browserAction.setBadgeBackgroundColor({color: "#0066CD"});
-        chrome.browserAction.setIcon({ path:
-          {
-            "16": "/images/icon16.png",
-            "48": "/images/icon48.png",
-            "128": "/images/icon128.png"
-          }
+        //do something, removing the script or whatever
+      } else if (data.state === 1){
+        chrome.storage.local.set({'state': 2, 'sound': true, 'pm': true}, function() {
+          chrome.browserAction.setBadgeText({text: "ON"});
+          chrome.browserAction.setBadgeBackgroundColor({color: "#0066CD"});
+          chrome.browserAction.setIcon({ path:
+            {
+              "16": "/images/icon16.png",
+              "48": "/images/icon48.png",
+              "128": "/images/icon128.png"
+            }
+          });
+          chrome.browserAction.setTitle({ title: 'sociotime' });
         });
-        chrome.browserAction.setTitle({ title: 'sociotime' });
-      });
-      // chrome.tabs.executeScript(null, { file: 'content.js' });
+        // chrome.tabs.executeScript(null, { file: 'content.js' });
 
-    } else {
-      chrome.storage.sync.set({'state': 0, 'sound': false}, function() {
-        chrome.browserAction.setBadgeBackgroundColor({color: "#000000"});
-        chrome.browserAction.setBadgeText({text: "MUTE"});
-      });
-    }
+      } else {
+        chrome.storage.local.set({'state': 0, 'sound': false}, function() {
+          chrome.browserAction.setBadgeBackgroundColor({color: "#000000"});
+          chrome.browserAction.setBadgeText({text: "MUTE"});
+        });
+      }
+    });
   });
-});
--- contentz.js
+++ contentz.js
@@ -12,48 +12,175 @@ $(document).ready(function() {
       return;
     // pick name
     if (event.data.type && (event.data.type == "FROM_CHROME_EXT")) {
+
+      // basic settings
+      var audio = new Audio('http://princezze.free.fr/sounds/Opbeep.wav');
+      var str = '';
+      var html = '';
+      var maxwidth = $('#leftdiv').outerWidth();
+      var maxhei = $('#main_box').outerHeight()/3;
+      $('#leftdiv').on('resize', function() {
+        maxwidth = $('#leftdiv').outerWidth();
+        maxhei = $('#main_box').outerHeight()/3;
+      })
+      audio.loop = false;
+
+      // fix images before start
+      $( "#leftdiv div" ).children().find("img").each(function() {
+        var src = $(this).attr("src").substring(0, 5);
+        if(src !== '/img/') $(this).addClass("img-responsive").wrap( "<span style='max-width:"+maxwidth+"px;max-height="+maxhei+"px'></span>" );
+      })
+      $( "#leftdiv div" ).html(function(){
+        return $(this).html().replace(/<br>/g," ");
+      });
+
+      // save last html
+      var html = $( "#leftdiv div" ).last().html();
+
       var myNameIs = regexEscape(event.data.text);
-      chrome.storage.sync.set({'name': myNameIs}, function() {
+      chrome.storage.local.set({'name': myNameIs, 'lastmsg': html}, function() {
         // remove injection
         $('#killmeplz').remove();
 
-        var audio = new Audio('http://princezze.free.fr/sounds/Opbeep.wav');
-        var str = '';
-        var html = '';
-        var maxwidth = $('#leftdiv').outerWidth();
-        $('#leftdiv').on('resize', function() {
-          maxwidth = $('#leftdiv').outerWidth();
-        })
-        audio.loop = false;
+        // выбираем целевой элемент
+        var target = document.getElementById('leftdiv');
+        // console.log(document.getElementById('leftdiv').innerHTML.substring(0,200));
+
+        // создаём экземпляр MutationObserver
+        var observer = new MutationObserver(function(mutations) {
+          var current = $( "#leftdiv div" ).last();
+          console.log(current);
+          mutations.forEach(function(mutation) {
+            console.log(mutation.type);
+          });
+        });
+
+        // конфигурация нашего observer:
+        var config = {
+          childList: true
+        };
+
+        // передаём в качестве аргументов целевой элемент и его конфигурацию
+        observer.observe(target, config);
+
+        // позже можно остановить наблюдение
+        // observer.disconnect();
+
+
+        // start messages cycle
+        /*
+        $('#leftdiv').on('DOMNodeInserted', function(event) {
+          chrome.storage.local.get(null, function (myresults) {
+            //pick next line
+            var current = $( "#leftdiv div" ).last();
+
+            html = current.html();
+
+            if(html!==myresults.lastmsg){
+              current.html(function(){
+                return $(this).html().replace(/<br>/g," ").replace(/\s{2,}/g," ");
+              });
+              chrome.storage.local.set({'lastmsg': html}, function() {
+
+                console.log(JSON.stringify(myresults));
+              });
+
+            }else console.log('Одинаковое!!');
+
+            // DEBUG LOG
+            // console.log(JSON.stringify(myresults));
+        });});
+        */
+      });
+    }
+  }, false);
+});
+/*
+
+
         $('#leftdiv').bind('DOMNodeInserted', function(event) {
-          chrome.storage.sync.get(null, function (myresults) {
-            // alert(JSON.stringify(myresults));
-            // alert('STATE '+myresults.state);
-            // if(myresults.pm) alert('ON ALL');
-            // if(myresults.sound) alert('ON SOUND');
-            $( "#leftdiv div" ).last().hide().show(800);
-            if(myresults.pm){
-              str = $( "#leftdiv div" ).last().text();
-              html = $( "#leftdiv div" ).last().html();
-              if(/<img/.test(html)) $( "#leftdiv div" ).last().children().find( "img" ).addClass("img-responsive").wrap( "<div style='max-width:"+maxwidth+"px'></div>" );
-              var regex = new RegExp(myresults.name, "gi");
-              if(regex.test(str)){
-                if(!$('#myownlog').length){
-                  $( "#privatdiv" ).after( '<div id="myownlog"></div>' );
-                }
-                $('#myownlog').append('<div class="js-killlog">'+str+'</div>');
-                $('#myownlog div').last().hide().show(200);
-                $('.js-killlog').on('click', function(e) {
-                  $(this).hide(500);
-                });
-                // console.log(str);
-                if(myresults.sound) audio.play();
-              }
-              if(myresults.sound){
-                $( "#leftdiv img" ).hide();
-                $( "#leftdiv canvas" ).parent().parent().html("");
-              }
+          chrome.storage.local.get(null, function (myresults) {
+
+            alert(JSON.stringify(myresults));
+            alert('STATE '+myresults.state);
+            if(myresults.pm) alert('ON ALL');
+            if(myresults.sound) alert('ON SOUND');
+
+            // $( "#leftdiv div" ).last().hide().show(800);
+
+            // save new string
+            var current = $( "#leftdiv div" ).last();
+
+            // remove br
+            current.html(function(){
+              return $(this).html().replace(/<br>/g," ");
+            });
+
+            // wrap image
+            if(/<img/g.test(html)){
+              current.children().find("img").each(function() {
+                var src = $(this).attr("src").substring(0, 5);
+                if(src !== '/img/' && !$(this).hasClass( "img-responsive" )) $(this).addClass("img-responsive").wrap( "<span style='max-width:"+maxwidth+"px;max-height="+maxhei+"px'></span>" );
+              })
             }
+
+            html = current.html();
+
+            //protection from doubles
+            if(myresults.lastmsg!=html)
+              chrome.storage.local.set({'lastmsg': html}, function() {
+                console.log(myresults.lastmsg+"\n=============="+html);
+                // slow shows last message
+
+
+                /*
+
+
+                // create one more block if hasnt
+                if(!$('#myownlog').length) $( "#privatdiv" ).after( '<div id="myownlog"></div>' );
+                // test text and save if needed
+                str = current.text();
+                var regex = new RegExp(myresults.name, "gi");
+                if(regex.test(str)){
+                  $('#myownlog').append('<div class="js-killlog">'+str+'</div>');
+                  $('#myownlog div').last().hide().show(200);
+                  $('.js-killlog').on('click', function(e) {
+                    $(this).hide(500);
+                  });
+                  // console.log(str);
+                  audio.play();
+
+                }
+
+              });
+
+            if(myresults.pm){
+
+              if(myresults.sound){
+                str = $( "#leftdiv div" ).last().text();
+                var regex = new RegExp(myresults.name, "gi");
+                if(regex.test(str)){
+                  $('#myownlog').append('<div class="js-killlog">'+str+'</div>');
+                  $('#myownlog div').last().hide().show(200);
+                  $('.js-killlog').on('click', function(e) {
+                    $(this).hide(500);
+                  });
+                  // console.log(str);
+                  audio.play();
+                }
+              }
+              //hide images
+              if(/<img/.test(html)){
+                // alert(current.children('img'));
+                current.children('img').replaceWith(function(){
+                  var urlimg = $(this).attr('src');
+                  return '<span class="ext-showimg" data='+urlimg+'>🖼 Показать изображение</span>';
+                });
+              }
+
+              $( "#leftdiv canvas" ).parent().parent().html("");
+            }
+
             while ($( "#leftdiv div" ).length > 200){
               $( "#leftdiv div" ).first().remove(500);
             }
@@ -63,7 +190,7 @@ $(document).ready(function() {
     }
   }, false);
 });
-
+*/
 function regexEscape(str) {
   return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
 }
